451ea5ef343a8913dbceaf3a35c6120e7d68898e
heerkubadia <117744600+heerkubadia@users.noreply.github.com>
2024-10-12 19:26:37 +0530

    Add files via upload

---
+++ tests_asm/sw-lw.asm
@@ -0,0 +1,24 @@
+
+main:
+    # Storing the first word
+    addi sp, sp, -4      
+    addi t0, zero, -3    # t0 = -3
+    sw t0, 0(sp)         
+
+    # Storing the second word
+    addi sp, sp, -4      
+    addi t1, zero, 5     #  t1 = 5
+    sw t1, 0(sp)         
+
+    # Loading the second word
+    lw t2, 0(sp)         # t2 = 5
+    addi sp, sp, 4      
+
+    # Loading the first word 
+    lw t3, 0(sp)         # t3 = -3
+    addi sp, sp, 4       
+    
+    # Performing add operation
+    add t4, t2, t3       # t4 = 2
+
+   
