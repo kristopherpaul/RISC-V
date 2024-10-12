e934500143a35336ecd29499dfbe01f4b5fd6f28
heerkubadia <117744600+heerkubadia@users.noreply.github.com>
2024-10-13 00:34:28 +0530

    Add files via upload

---
+++ tests_asm/sw-lb-lh-lw.asm
@@ -0,0 +1,26 @@
+main:
+    lui t0, 0xFFF8F  # t0= 0xFFF8F000
+
+    # Storing the word
+    addi sp, sp, -4     
+    sw t0, 0(sp)          
+    
+    # Loading the word
+    lw t1, 0(sp)         
+    
+    # Loading the halfword
+    lh t2, 0(sp)          
+    
+    # Loading the byte
+    lb t3, 0(sp)          
+    
+    #Loading the unsigned halfword
+    #lhu t4, 0(sp)
+    
+    #Loading the unsigned word
+    #lbu t5, 0(sp)
+    
+   #performing operations for sanity check   
+   add t3, t0,t1       
+   add t4, t1, t2	
+  
