db4553b3d9da4a92de9e72e139fb5c8dcbfcc5e7
heerkubadia <117744600+heerkubadia@users.noreply.github.com>
2024-10-15 03:46:22 +0530

    Add files via upload

---
+++ tests_asm/sw-lw-lwu.asm
@@ -0,0 +1,17 @@
+main:
+         
+    lui t0, 0xFFF8F  
+    
+    addi sp, sp, -4      
+    sw t0, 0(sp)         
+    
+    # Loading the word
+    lw t1, 0(sp)         
+    
+    # Loading the word unsigned
+    lwu t2, 0(sp)          
+    addi sp, sp, 4
+   
+
+   #performing operations for sanity check   
+   add t3, t1,t2      
