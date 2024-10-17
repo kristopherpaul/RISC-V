1c212852bd0c54923ae0ff7abf2bef26279e3f91
lavanya2504 <143703003+lavanya2504@users.noreply.github.com>
2024-10-17 20:02:01 +0530

    Add files via upload

---
+++ tests_asm/and.asm
@@ -0,0 +1,8 @@
+main:
+   
+    addi t0, zero, 1024
+    addi t1, zero, 1232                
+    and t2, t0, t1                      
+    addi t3, zero, -1024
+    addi t4, zero, -1232                  
+    and  t5, t3, t4                      
