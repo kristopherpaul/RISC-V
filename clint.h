4a7add23b06e4daee83c4ec13ce7b033a067b525
Mrigankashekhar Shandilya <65168739+mshandilya@users.noreply.github.com>
2024-11-06 22:58:44 +0530

    Create clint.h

---
+++ clint.h
@@ -0,0 +1,21 @@
+#ifndef CLINT_H
+#define CLINT_H
+
+#include "utils.h"
+
+typedef struct {
+    u64 mtime;
+    u64 mtimecmp;
+} CLINT;
+
+extern CLINT clint;
+
+u64 clint_load(u64 addr, u64 size, Exception* exception)
+
+void clint_store(u64 addr, u64 size, u64 value, Exception* exception)
+
+u64 clint_load64(u64 addr)
+
+void clint_store64(u64 addr, u64 value)
+
+#endif
