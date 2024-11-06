bcd5d87f995b171f0f08c919874c5a8e46ce49a4
Mrigankashekhar Shandilya <65168739+mshandilya@users.noreply.github.com>
2024-11-06 23:00:49 +0530

    Update clint.c

--- clint.c
+++ clint.c
@@ -1,64 +1,40 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <stdlib.h>
+#include <clint.h>
 
-
-// typedef enum {
-//     LoadAccessFault,
-//     StoreAMOAccessFault,
-//     // Add other exceptions as needed
-// } Exception;
-
-
-typedef struct {
-    u64 mtime;
-    u64 mtimecmp;
-} Clint;
-
-Clint* clint_new() {
-    Clint* clint = (Clint*)malloc(sizeof(Clint));
-    if (clint) {
-        clint->mtime = 0;
-        clint->mtimecmp = 0;
-    }
-    return clint;
-}
-
-u64 clint_load(Clint* clint, u64 addr, u64 size, Exception* exception) {
+u64 clint_load(u64 addr, u64 size, Exception* exception) {
     if (size == 64) {
-        return clint_load64(clint, addr);
+        return clint_load64(addr);
     } else {
         *exception = LoadAccessFault;
         return 0;
     }
 }
 
-void clint_store(Clint* clint, u64 addr, u64 size, u64 value, Exception* exception) {
+void clint_store(u64 addr, u64 size, u64 value, Exception* exception) {
     if (size == 64) {
-        clint_store64(clint, addr, value);
+        clint_store64(addr, value);
     } else {
         *exception = StoreAMOAccessFault;
     }
 }
 
-u64 clint_load64(Clint* clint, u64 addr) {
+u64 clint_load64(u64 addr) {
     switch (addr) {
         case CLINT_MTIMECMP:
-            return clint->mtimecmp;
+            return clint.mtimecmp;
         case CLINT_MTIME:
-            return clint->mtime;
+            return clint.mtime;
         default:
             return 0;
     }
 }
 
-void clint_store64(Clint* clint, u64 addr, u64 value) {
+void clint_store64(u64 addr, u64 value) {
     switch (addr) {
         case CLINT_MTIMECMP:
-            clint->mtimecmp = value;
+            clint.mtimecmp = value;
             break;
         case CLINT_MTIME:
-            clint->mtime = value;
+            clint.mtime = value;
             break;
         default:
             break;
