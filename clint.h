0e6060a227aff4a8b95ce837f97f517bf8e2f54d
Mrigankashekhar Shandilya <65168739+mshandilya@users.noreply.github.com>
2024-11-06 23:08:52 +0530

    Modified clint.h

--- clint.h
+++ clint.h
@@ -3,19 +3,18 @@
 
 #include "utils.h"
 
-typedef struct {
+typedef struct CLINT {
     u64 mtime;
     u64 mtimecmp;
 } CLINT;
 
 extern CLINT clint;
 
-u64 clint_load(u64 addr, u64 size, Exception* exception)
+u64 clint_load(u64 addr, u64 size, Exception* exception);
 
-void clint_store(u64 addr, u64 size, u64 value, Exception* exception)
+void clint_store(u64 addr, u64 size, u64 value, Exception* exception);
 
-u64 clint_load64(u64 addr)
+u64 clint_load64(u64 addr);
 
-void clint_store64(u64 addr, u64 value)
-
+void clint_store64(u64 addr, u64 value);
 #endif
