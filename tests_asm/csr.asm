4de46e1cdfbee9ce502120f2d5350477e11db776
Mrigankashekhar Shandilya <65168739+mshandilya@users.noreply.github.com>
2024-11-12 04:25:02 +0530

    corrected csr test

--- tests_asm/csr.asm
+++ tests_asm/csr.asm
@@ -2,11 +2,12 @@ main:
   addi t0, zero, 1
   addi t1, zero, 2
   addi t2, zero, 3
-  csrrw zero, mstatus, t0
-  csrrs zero, mtvec, t1
-  csrrw zero, mepc, t2
-  csrrc t2, mepc, zero
-  csrrwi zero, sstatus, 4
-  csrrsi zero, stvec, 5
-  csrrwi zero, sepc, 6
-  csrrci zero, sepc, 0
+  csrrw zero, 0x300, t0
+  csrrs zero, 0x305, t1
+  csrrw zero, 0x341, t2
+  csrrc t2, 0x341, zero
+  csrrwi zero, 0x100, 4
+  csrrsi zero, 0x105, 5
+  csrrwi zero, 0x141, 6
+  csrrci zero, 0x141, 0
+  jr ra
